259637d4234967aa049c983f42c8fe449ff55d39
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-03-15 00:50:30 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+
+https://www.w3schools.com/cssref/pr_background-position.php
+
+https://paletadecores.com/
+
+https://horadecodar.com.br/2020/04/17/imagem-de-fundo-preenchendo-a-tela-inteira/
+
+https://www.w3schools.com/howto/howto_css_zoom_hover.asp
+
+https://desenvolvimentoparaweb.com/css/animacoes-css-introducao/
+
+*/
